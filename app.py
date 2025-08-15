981423a33a3502a550367cc7b74066259b85d7f7
Peter Lugalia <masaba.peter68@gmail.com>
2025-08-15 19:40:44 +0200

    edits made on registration html

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -15,6 +15,28 @@ def shop():
     types = ['12oz Medium Roast', '24oz French Roast', '96oz Whole Beans']
     return render_template('shop.html', types=types)
 
-@app.route('/register', methods=['GET'])
+@app.route('/register', methods=['GET','POST'])
 def register():
-    return render_template('register.html')
+    message = ""
+    error = []
+    if request.method == 'POST':
+        # Handle registration logic here
+        username = request.form['uname']
+        password = request.form['pword']
+        confirm = request.form['confirm']
+        if not username:
+            error.append("Username is required.")
+        if not password:
+            error.append("Password is required.")
+        if not confirm:
+            error.append("Password confirmation is required.")
+        if len(username) < 3:
+            error.append("Username must be at least 3 characters long.")
+        if password != confirm:
+            error.append("Passwords do not match.")
+        
+        if error:
+            message = "Registration failed"
+        else:
+            message = f"Successfully registered {username}"
+    return render_template('register.html', message=message, error= error)
