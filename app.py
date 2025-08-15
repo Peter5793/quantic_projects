09416a96bb62dc93da7d2f2403d374665f30def7
Peter Lugalia <masaba.peter68@gmail.com>
2025-08-15 14:55:58 +0200

    add the register code

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def welcome():
-    return "Welcome to Wholly Roasters"
+    return render_template('home.html')
 
 @app.route('/about', methods=['GET'])
 def about():
-    return "Wholly Roasters is a coffee shop dedicated to serving the best coffee in town."
+    return render_template('about.html')
+
+@app.route('/shop', methods=['GET'])
+def shop():
+    types = ['12oz Medium Roast', '24oz French Roast', '96oz Whole Beans']
+    return render_template('shop.html', types=types)
+
+@app.route('/register', methods=['GET'])
+def register():
+    return render_template('register.html')
